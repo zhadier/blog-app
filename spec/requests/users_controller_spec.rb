dce3493740e9ad85106359f9cc0c62ee22fa4722
Zeeshan Haider <zhadier39@gmail.com>
2022-04-28 00:58:48 +0500

    Add specs for users_controller methods

---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  context 'GET #index' do
+    before(:example) { get users_path } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+
+    it 'does not render a different template' do
+      expect(response).to_not render_template(:show)
+    end
+
+    it 'has correct placeholder text' do
+      expect(response.body).to include('Inside users index')
+    end
+  end
+
+  context 'GET #show' do
+    before(:example) { get '/users/3'} # get(:show)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'does not render a different template' do
+      expect(response).to_not render_template(:index)
+    end
+
+    it 'has correct placeholder text' do
+      expect(response.body).to include('Inside users show')
+    end
+  end
+end
