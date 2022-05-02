86cdc769ccfd1dcf8c4bf77a9851306e356d55f7
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 02:54:56 +0500

    Add posts query methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count())
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
 end
