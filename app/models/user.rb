f86b1fa6f8776e008811ab4821800e4dffb20e8c
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 02:03:29 +0500

    Add seeds and add correct foreign keys and Classnames for Author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :likes
-  has_many :comments
-  
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  scope :recent_posts, -> (id) { find(id).posts.order(3, :desc)limit(3) }
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author: second_user, title: 'Hello', text: 'This is the fourth post')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Lisandro!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Sahar!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Andres!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Nicolas!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Reem!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Other Stalkers!' )
