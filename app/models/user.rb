dbe67b6c179f404e84d93bfe9e379101bfa3cf98
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 02:54:20 +0500

    Add user query methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-  scope :recent_posts, -> (id) { find(id).posts.order(3, :desc)limit(3) }
 
-
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
