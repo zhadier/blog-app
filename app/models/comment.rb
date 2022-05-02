c1b0c80719cbaf1ed57fb7009f5dacfc86226def
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 02:54:36 +0500

    Add comments query methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :post
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count())
+  end
 end
