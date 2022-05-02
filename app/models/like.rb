7268474830cb239afbc5b5d4e28dd785adabb1e1
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 02:54:45 +0500

    Add likes query methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :post
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count())
+  end
 end
