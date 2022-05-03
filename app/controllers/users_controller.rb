2e2cd047beae5a81a39ba72c714035f6af32987c
Zeeshan Haider <zhadier39@gmail.com>
2022-05-03 22:18:32 +0500

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @user = User.all
+  end
 
-  def show; end
+  def show
+    @user_current = User.find(params[:id])
+  end
 end
