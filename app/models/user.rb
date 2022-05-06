7c035aa744b4143015d4f19db76ed286609caf6f
Zeeshan Haider <zhadier39@gmail.com>
2022-05-06 19:09:46 +0500

    Update users model to validate attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
-  after_create do
-    update(posts_counter: 0)
+  before_create do
+    self.posts_counter = 0
   end
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.includes(:author, :likes, :comments).order(created_at: :desc).limit(3)
   end
 end
