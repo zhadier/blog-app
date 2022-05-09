6592a212724b1ad80498d0145f3b2caa9ba9e5ef
Zeeshan Haider <zhadier39@gmail.com>
2022-05-10 02:11:29 +0500

    Add devise and set it up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
@@ -6,7 +10,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   before_create do
-    self.posts_counter = 0
+    posts_counter = 0
   end
 
   def recent_posts
