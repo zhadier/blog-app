bf495f0a4410c2d9a3660af97723f0f9bbebd9e4
Zeeshan Haider <zhadier39@gmail.com>
2022-05-10 04:26:08 +0500

    Fix small bug in model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :email, :password, :current_password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   before_create do
-    posts_counter = 0
+    self.posts_counter = 0
   end
 
   def recent_posts
