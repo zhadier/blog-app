dafce6ebebd4a2fe32f4e8d0a2f502ec66464a00
Piero Lescano <pierolescano@protonmail.com>
2022-05-10 16:24:27 -0500

    Fix error when creating new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,13 @@ class CommentsController < ApplicationController
   load_and_authorize_resource
   def create
     @post = Post.find(params[:post_id])
-    respond_to do |format|
-      format.html do
-        values = params.require(:comment).permit(:text)
-        @new_comment = Comment.new(post: @post, author: current_user, text: values[:text])
-        if @new_comment.save
-          flash[:success] = 'Comment was saved'
-        else
-          flash[:error] = 'Failed to add comment'
-        end
-        redirect_back(fallback_location: root_path)
-      end
+    @new_comment = Comment.new(comment_params.merge(post_id: @post.id, author_id: current_user.id))
+    if @new_comment.save
+      flash[:success] = 'Comment was saved'
+    else
+      flash[:error] = 'Failed to add comment'
     end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -21,5 +16,11 @@ class CommentsController < ApplicationController
     @comment.destroy
     flash[:success] = 'Comment was deleted'
     redirect_to user_post_path(params[:user_id], params[:post_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
