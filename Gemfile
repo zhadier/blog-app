704132eb1adb799b317051efe2be7eafb51822c1
Zeeshan Haider <zhadier39@gmail.com>
2022-05-11 23:00:44 +0500

    Add feature tests for Login page

--- Gemfile
+++ Gemfile
@@ -78,6 +78,14 @@ group :development do
   # gem "spring"
 end
 
+# Add Rspec
+# Run against this stable release
+group :development, :test do
+  gem 'rails-controller-testing'
+  gem 'rspec-rails', '~> 6.0.0.rc1'
+  gem 'database_cleaner'
+end
+
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
@@ -85,9 +93,4 @@ group :test do
   gem 'webdrivers'
 end
 
-# Add Rspec
-# Run against this stable release
-group :development, :test do
-  gem 'rails-controller-testing'
-  gem 'rspec-rails', '~> 6.0.0.rc1'
-end
+
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,3 +62,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Capybara.default_driver = :selenium_chrome
